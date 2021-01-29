a8c123c6c3395f2c1950615e7f5995fcea1a11d4
Davey Moores <daveymoores@gmail.com>
2021-01-29 07:36:08 +0100

    animating with framer mation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion, useCycle, AnimateSharedLayout } from "framer-motion";
+import "./App.css";
 
-function App() {
+interface PoleProps {
+  index: number;
+  style: any;
+  parentIndex: number;
+}
+
+const items = [0, 1, 2];
+const colors = ["pink", "hotpink", "yellow"];
+
+const animationSequences = [
+  [
+    { rotate: 0, originY: 0.125, originX: 0.5 },
+    { rotate: 90, originY: 0.125, originX: 0.5 },
+    { rotate: 90, originY: 0.125, originX: 0.5 },
+  ],
+  [
+    { rotate: 0, originY: 0.125, originX: 0.5 },
+    { rotate: 180, originY: 0.125, originX: 0.5 },
+    { rotate: 180, originY: 0.125, originX: 0.5 },
+  ],
+  [
+    { rotate: 0, originY: 0.125, originX: 0.5 },
+    { rotate: 90, originY: 0.125, originX: 0.5 },
+    { rotate: 45, originY: 0.875, originX: 0 },
+  ],
+];
+
+const Pole = ({ index, style, parentIndex }: PoleProps) => {
+  const [animate, cycle] = useCycle(...animationSequences[index]);
+
+  React.useEffect(() => {
+    cycle(parentIndex);
+  }, [parentIndex, cycle]);
+
+  return (
+    <motion.div
+      className={"pole"}
+      animate={animate}
+      transition={{
+        duration: 0.4,
+        ease: "easeInOut",
+      }}
+      style={style}
+    />
+  );
+};
+
+const App = () => {
+  const [parentIndex, setParentIndex] = React.useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        className={"container"}
+        onTap={() =>
+          setParentIndex((index) =>
+            index + 1 > animationSequences[0].length - 1 ? 0 : index + 1
+          )
+        }
+      >
+        <AnimateSharedLayout>
+          {items.map((item) => (
+            <Pole
+              style={{ backgroundColor: colors[item] }}
+              key={item}
+              parentIndex={parentIndex}
+              index={item}
+            />
+          ))}
+        </AnimateSharedLayout>
+      </motion.div>
     </div>
   );
-}
+};
 
 export default App;
