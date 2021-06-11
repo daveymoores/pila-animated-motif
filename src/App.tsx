fabda6f1497592ae4652e82dba5661469818bfd8
Davey Moores <daveymoores@gmail.com>
2021-06-11 15:19:36 +0200

    using dom matrix

--- src/App.tsx
+++ src/App.tsx
@@ -1,124 +1,58 @@
 import React from "react";
-import { motion, useCycle } from "framer-motion";
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
 import "./App.css";
-
-interface PoleProps {
-  index: number;
-  style?: any;
-  parentIndex: number;
-}
 
 interface MotifProps {
   className?: string;
 }
 
-const items = [0, 1];
+const parseMatrixValue = (matrixValue: string) =>
+  matrixValue.indexOf("e") > 0 ? 0 : parseFloat(matrixValue);
 
-const animationSequences = [
-  [
-    { rotate: 0, originY: 0.125, originX: 0.5 },
-    { rotate: 90, originY: 0.125, originX: 0.5 },
-    { rotate: 90, originY: 0.125, originX: 0.5 },
-    { rotate: 90, originY: 0.125, originX: 0.5 },
-    { rotate: 90, originY: 0.125, originX: 0.5 },
-    { rotate: 90, originY: 0.125, originX: 0.5 },
-    { rotate: 90, originY: 0.125, originX: 0.5 },
-  ],
-  [
-    { rotate: 0, originY: 0.125, originX: 0.5 },
-    { rotate: 180, originY: 0.125, originX: 0.5 },
-    { rotate: 180, originY: 0.125, originX: 0.5 },
-    { rotate: 180, originY: 0.125, originX: 0.5 },
-    { rotate: 180, originY: 0.125, originX: 0.5 },
-    { rotate: 180, originY: 0.125, originX: 0.5 },
-    { rotate: 180, originY: 0.125, originX: 0.5 },
-  ],
-];
+const Pole = () => {
+  const [matrix, setMatrix] = React.useState(new DOMMatrix().toString());
 
-const animationSequences2 = [
-  [
-    { rotate: 0, originY: 0.5, originX: 0.125 },
-    { rotate: 0, originY: 0.5, originX: 0.125 },
-    { rotate: 90, originY: 0.5, originX: 0.125 },
-    { rotate: -90, originY: 0.5, originX: 0.125 },
-    { rotate: -90, originY: 0.5, originX: 0.125 },
-    { rotate: -90, originY: 0.5, originX: 0.125 },
-    { rotate: 0, originY: 0.5, originX: 0.125 },
-  ],
-];
-
-const animationSequences3 = [
-  [
-    { rotate: 0, originY: 0.125, originX: 0.5 },
-    { rotate: 0, originY: 0.125, originX: 0.5 },
-    { rotate: 0, originY: 0.125, originX: 0.5 },
-    { rotate: 0, originY: 0.125, originX: 0.5 },
-    { rotate: -90, originY: 0.125, originX: 0.5 },
-    { rotate: 0, originY: 0.125, originX: 0.5 },
-    { rotate: 0, originY: 0.125, originX: 0.5 },
-  ],
-];
-
-const Pole3 = ({ index, parentIndex }: PoleProps) => {
-  const [animate, cycle] = useCycle(...animationSequences3[index]);
+  const a = useSpring(1);
+  const b = useSpring(0);
+  const c = useSpring(0);
+  const d = useSpring(1);
+  const e = useSpring(0);
+  const f = useSpring(0);
 
   React.useEffect(() => {
-    cycle(parentIndex);
-  }, [parentIndex, cycle]);
+    const matrixValues = matrix
+      .split(",")
+      .map((x) =>
+        parseMatrixValue(x.replace(/[matrix]/gi, "").replace(/([()])/g, ""))
+      );
+    console.log("matrixValues ", matrixValues);
+    a.set(matrixValues[0]);
+    b.set(matrixValues[1]);
+    c.set(matrixValues[2]);
+    d.set(matrixValues[3]);
+    e.set(matrixValues[4]);
+    f.set(matrixValues[5]);
+  }, [matrix, a, b, c, d, e, f]);
 
-  return (
-    <div
-      className={"pole3-container"}
-      style={{ opacity: parentIndex >= 4 && parentIndex <= 5 ? 1 : 0 }}
-    >
-      <motion.div
-        className={"pole pole3"}
-        animate={animate}
-        transition={{
-          duration: 0.4,
-          ease: "easeInOut",
-        }}
-      />
-    </div>
-  );
-};
-
-const Pole2 = ({ index, parentIndex, style }: PoleProps) => {
-  const [animate, cycle] = useCycle(...animationSequences2[index]);
-
-  React.useEffect(() => {
-    cycle(parentIndex);
-  }, [parentIndex, cycle]);
-
-  return (
-    <div
-      className={"pole2-container"}
-      style={{ opacity: parentIndex >= 2 ? 1 : 0 }}
-    >
-      <motion.div
-        className={"pole pole2"}
-        animate={animate}
-        transition={{
-          duration: 0.4,
-          ease: "easeInOut",
-        }}
-        style={style}
-      />
-    </div>
-  );
-};
-
-const Pole = ({ index, parentIndex }: PoleProps) => {
-  const [animate, cycle] = useCycle(...animationSequences[index]);
-
-  React.useEffect(() => {
-    cycle(parentIndex);
-  }, [parentIndex, cycle]);
+  const onInteractionHandler = () => {
+    // have to change it to string as react can't diff DOMMatrix
+    setMatrix((domMatrix) => {
+      return new DOMMatrix(domMatrix).toString();
+    });
+  };
 
   return (
     <motion.div
       className={"pole"}
-      animate={animate}
+      onClick={onInteractionHandler}
+      style={{
+        transform: useMotionTemplate`matrix(${a}, ${b}, ${c}, ${d}, ${e}, ${f})`,
+      }}
       transition={{
         duration: 0.4,
         ease: "easeInOut",
@@ -127,34 +61,11 @@ const Pole = ({ index, parentIndex }: PoleProps) => {
   );
 };
 
-const getRandomInt = (max: number) => {
-  return Math.floor(Math.random() * Math.floor(max));
-};
-
 const Motif = ({ className }: MotifProps) => {
-  const sequenceLength = animationSequences[0].length;
-
-  const [parentIndex, setParentIndex] = React.useState(getRandomInt(0));
-
-  const onInteractionHandler = React.useCallback(
-    () =>
-      setParentIndex((index) =>
-        index + 1 > sequenceLength - 1 ? 0 : index + 1
-      ),
-    []
-  );
-
   return (
-    <motion.div
-      className={`container ${className}`}
-      onMouseEnter={onInteractionHandler}
-    >
+    <motion.div className={`container ${className}`}>
       <div className={"pole_wrapper"}>
-        {items.map((item) => (
-          <Pole key={item} parentIndex={parentIndex} index={item} />
-        ))}
-        <Pole2 parentIndex={parentIndex} index={0} />
-        <Pole3 parentIndex={parentIndex} index={0} />
+        <Pole />
       </div>
     </motion.div>
   );
@@ -164,14 +75,7 @@ const App = () => {
   return (
     <div className="App">
       <div className={"motif_wrapper motif_wrapper__overlay"}>
-        {[...Array(9)].map(() => {
-          return <Motif className={"green"} />;
-        })}
-      </div>
-      <div className={"motif_wrapper"}>
-        {[...Array(80)].map(() => {
-          return <Motif />;
-        })}
+        <Motif className={"green"} />
       </div>
     </div>
   );
